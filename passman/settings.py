721530f22f501b3cc38214d19ffe882e2d0ed5dc
adiziel <aditya.pathak.plus@gmail.com>
2021-11-01 03:53:51 +0530

    database change

--- passman/settings.py
+++ passman/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'passman.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'passman-db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
